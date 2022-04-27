72018431b1a692908bbc970bf74b8837cbf8f167
mnjrupp <mnjrupp@hotmail.com>
2022-04-27 09:15:56 -0500

    2 files modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {BasiccheckComponent} from './basiccheck/basiccheck.component';
+import {AdvancecheckComponent} from './advancecheck/advancecheck.component';
+import {FinalcheckComponent} from './finalcheck/finalcheck.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,BasiccheckComponent,AdvancecheckComponent,FinalcheckComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
